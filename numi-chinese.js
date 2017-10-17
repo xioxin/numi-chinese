fbcfb294805709b1ae948f792dcc8f6cf5a88fbd
xioxin <i@xioxin.com>
2017-10-16 20:30:33 -0500

    Create numi-chinese.js

---
+++ numi-chinese.js
@@ -0,0 +1,77 @@
+
+//时间
+numi.addUnit({
+   "id": "年",
+   "phrases": "年",
+   "baseUnitId": "second",
+   "format" : "年",
+   "ratio" : 31536000,
+});
+numi.addUnit({
+   "id": "天",
+   "phrases": "天,日",
+   "baseUnitId": "second",
+   "format" : "天",
+   "ratio" : 86400,
+});
+numi.addUnit({
+   "id": "小时",
+   "phrases": "小时",
+   "baseUnitId": "second",
+   "format" : "小时",
+   "ratio" : 3600,
+});
+numi.addUnit({
+   "id": "周",
+   "phrases": "周",
+   "baseUnitId": "second",
+   "format" : "周",
+   "ratio" : 604800,
+});
+numi.addUnit({
+   "id": "分钟",
+   "phrases": "分钟,分",
+   "baseUnitId": "second",
+   "format" : "分钟",
+   "ratio" : 60,
+});
+numi.addUnit({
+   "id": "秒",
+   "phrases": "秒",
+   "baseUnitId": "second",
+   "format" : "秒",
+   "ratio" : 1,
+});
+
+
+
+
+//距离
+numi.addUnit({
+    "id": "千米",
+    "phrases": "千米,公里",
+    "baseUnitId": "m",
+    "format" : "米",
+    "ratio" : 1000,
+});
+numi.addUnit({
+    "id": "米",
+    "phrases": "米",
+    "baseUnitId": "m",
+    "format" : "米",
+    "ratio" : 1,
+});
+numi.addUnit({
+    "id": "分米",
+    "phrases": "分米",
+    "baseUnitId": "cm",
+    "format" : "分米",
+    "ratio" : 0.1,
+});
+numi.addUnit({
+    "id": "厘米",
+    "phrases": "厘米",
+    "baseUnitId": "m",
+    "format" : "厘米",
+    "ratio" : 0.01,
+});
