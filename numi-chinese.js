1af9a6330f4e828bcfeb855b3c833257a84c7c32
xioxin <i@xioxin.com>
2017-10-16 20:54:49 -0500

    Update numi-chinese.js

--- numi-chinese.js
+++ numi-chinese.js
@@ -30,7 +30,7 @@ numi.addUnit({
 });
 numi.addUnit({
    "id": "分钟",
-   "phrases": "分钟,分",
+   "phrases": "分钟",
    "baseUnitId": "second",
    "format" : "分钟",
    "ratio" : 60,
@@ -42,11 +42,33 @@ numi.addUnit({
    "format" : "秒",
    "ratio" : 1,
 });
+numi.addUnit({
+   "id": "毫秒",
+   "phrases": "毫秒",
+   "baseUnitId": "second",
+   "format" : "毫秒",
+   "ratio" : 0.001,
+});
+numi.addUnit({
+   "id": "微妙",
+   "phrases": "微妙",
+   "baseUnitId": "second",
+   "format" : "微妙",
+   "ratio" : 1e-6,
+});
+numi.addUnit({
+   "id": "纳秒",
+   "phrases": "纳秒",
+   "baseUnitId": "second",
+   "format" : "纳秒",
+   "ratio" : 1e-9,
+});
 
 
 
 
-//距离
+
+//长度
 numi.addUnit({
     "id": "千米",
     "phrases": "千米,公里",
@@ -75,3 +97,303 @@ numi.addUnit({
     "format" : "厘米",
     "ratio" : 0.01,
 });
+numi.addUnit({
+    "id": "毫米",
+    "phrases": "毫米",
+    "baseUnitId": "m",
+    "format" : "毫米",
+    "ratio" : 0.001,
+});
+numi.addUnit({
+    "id": "微米",
+    "phrases": "微米",
+    "baseUnitId": "m",
+    "format" : "微米",
+    "ratio" : 1e-6,
+});
+numi.addUnit({
+    "id": "纳米",
+    "phrases": "纳米",
+    "baseUnitId": "m",
+    "format" : "纳米",
+    "ratio" : 1e-9,
+});
+numi.addUnit({
+    "id": "皮米",
+    "phrases": "皮米",
+    "baseUnitId": "m",
+    "format" : "皮米",
+    "ratio" : 1e-12,
+});
+numi.addUnit({
+    "id": "光年",
+    "phrases": "光年",
+    "baseUnitId": "m",
+    "format" : "光年",
+    "ratio" : 9.4607e+15,
+});
+numi.addUnit({
+    "id": "天文单位",
+    "phrases": "天文单位",
+    "baseUnitId": "m",
+    "format" : "天文单位",
+    "ratio" : 149600000000,
+});
+numi.addUnit({
+    "id": "英寸",
+    "phrases": "英寸",
+    "baseUnitId": "m",
+    "format" : "英寸",
+    "ratio" : 0.0254,
+});
+numi.addUnit({
+    "id": "码",
+    "phrases": "码",
+    "baseUnitId": "m",
+    "format" : "码",
+    "ratio" :0.9144,
+});
+numi.addUnit({
+    "id": "英里",
+    "phrases": "英里",
+    "baseUnitId": "m",
+    "format" : "英里",
+    "ratio" :1609.344,
+});
+numi.addUnit({
+    "id": "海里",
+    "phrases": "海里",
+    "baseUnitId": "m",
+    "format" : "海里",
+    "ratio" :1852,
+});
+numi.addUnit({
+    "id": "英寻",
+    "phrases": "英寻",
+    "baseUnitId": "m",
+    "format" : "英寻",
+    "ratio" :1.8288,
+});
+numi.addUnit({
+    "id": "弗隆",
+    "phrases": "弗隆",
+    "baseUnitId": "m",
+    "format" : "弗隆",
+    "ratio" :201.168,
+});
+
+numi.addUnit({
+    "id": "里",
+    "phrases": "里",
+    "baseUnitId": "m",
+    "format" : "里",
+    "ratio" :500,
+});
+numi.addUnit({
+    "id": "丈",
+    "phrases": "丈",
+    "baseUnitId": "m",
+    "format" : "丈",
+    "ratio" :10/3,
+});
+numi.addUnit({
+    "id": "尺",
+    "phrases": "尺",
+    "baseUnitId": "m",
+    "format" : "尺",
+    "ratio" :1/3,
+});
+numi.addUnit({
+    "id": "寸",
+    "phrases": "寸",
+    "baseUnitId": "m",
+    "format" : "寸",
+    "ratio" :0.1/3,
+});
+numi.addUnit({
+    "id": "分-长度",
+    "phrases": "分",
+    "baseUnitId": "m",
+    "format" : "分",
+    "ratio" :0.01/3,
+});
+numi.addUnit({
+    "id": "厘",
+    "phrases": "厘",
+    "baseUnitId": "m",
+    "format" : "厘",
+    "ratio" :0.001/3,
+});
+numi.addUnit({
+    "id": "毫",
+    "phrases": "毫",
+    "baseUnitId": "m",
+    "format" : "毫",
+    "ratio" :0.0001/3,
+});
+
+//面积
+numi.addUnit({
+    "id": "平方千米",
+    "phrases": "平方千米",
+    "baseUnitId": "m2",
+    "format" : "平方千米",
+    "ratio" :1000000,
+});
+numi.addUnit({
+    "id": "公顷",
+    "phrases": "公顷",
+    "baseUnitId": "m2",
+    "format" : "公顷",
+    "ratio" :10000,
+});
+numi.addUnit({
+    "id": "公亩",
+    "phrases": "公亩",
+    "baseUnitId": "m2",
+    "format" : "公亩",
+    "ratio" :100,
+});
+numi.addUnit({
+    "id": "平方米",
+    "phrases": "平方米",
+    "baseUnitId": "m2",
+    "format" : "平方米",
+    "ratio" :1,
+});
+numi.addUnit({
+    "id": "平方分米",
+    "phrases": "平方分米",
+    "baseUnitId": "m2",
+    "format" : "平方分米",
+    "ratio" :0.01,
+});
+numi.addUnit({
+    "id": "平方厘米",
+    "phrases": "平方厘米",
+    "baseUnitId": "m2",
+    "format" : "平方厘米",
+    "ratio" :0.0001,
+});
+numi.addUnit({
+    "id": "平方毫米",
+    "phrases": "平方毫米",
+    "baseUnitId": "m2",
+    "format" : "平方毫米",
+    "ratio" :1e-6,
+});
+numi.addUnit({
+    "id": "英亩",
+    "phrases": "英亩",
+    "baseUnitId": "m2",
+    "format" : "英亩",
+    "ratio" :4046.8564224,
+});
+numi.addUnit({
+    "id": "平方英里",
+    "phrases": "平方英里",
+    "baseUnitId": "m2",
+    "format" : "平方英里",
+    "ratio" :2589988.110336,
+});
+numi.addUnit({
+    "id": "平方码",
+    "phrases": "平方码",
+    "baseUnitId": "m2",
+    "format" : "平方码",
+    "ratio" :0.8361274,
+});
+numi.addUnit({
+    "id": "平方英尺",
+    "phrases": "平方英尺",
+    "baseUnitId": "m2",
+    "format" : "平方英尺",
+    "ratio" :0.092903,
+});
+numi.addUnit({
+    "id": "平方英寸",
+    "phrases": "平方英寸",
+    "baseUnitId": "m2",
+    "format" : "平方英寸",
+    "ratio" :0.0006452,
+});
+numi.addUnit({
+    "id": "平方竿",
+    "phrases": "平方竿",
+    "baseUnitId": "m2",
+    "format" : "平方竿",
+    "ratio" :25.2928526,
+});
+numi.addUnit({
+    "id": "顷",
+    "phrases": "顷",
+    "baseUnitId": "m2",
+    "format" : "顷",
+    "ratio" :66666.6666667,
+});
+numi.addUnit({
+    "id": "亩",
+    "phrases": "亩",
+    "baseUnitId": "m2",
+    "format" : "亩",
+    "ratio" :666.6666667,
+});
+numi.addUnit({
+    "id": "分-面积",
+    "phrases": "分",
+    "baseUnitId": "m2",
+    "format" : "分",
+    "ratio" :66.6666667,
+});
+numi.addUnit({
+    "id": "平方尺",
+    "phrases": "平方尺",
+    "baseUnitId": "m2",
+    "format" : "平方尺",
+    "ratio" :0.1111111,
+});
+numi.addUnit({
+    "id": "平方寸",
+    "phrases": "平方寸",
+    "baseUnitId": "m2",
+    "format" : "平方寸",
+    "ratio" :0.0011111,
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
